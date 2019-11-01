b963296cc39e78fb705cd166e774c076318f11bf
SpencerLee <spacecar@kali.pi.hole>
2019-11-01 16:17:00 -0400

    validation logic

--- travel.cmdctrl.ca/validate.php
+++ travel.cmdctrl.ca/validate.php
@@ -1,7 +1,8 @@
 <?php
-include "securesession.php"
+include "securesession.php";
 if (!isset($_SESSION['user']) && !isset($_SESSION["userid"])){
 	header("Location: https://travel.cmdctrl.ca:8443");
 }
+//<?php include "validate.php"; ?>
 
 ?>
