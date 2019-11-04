990c831b2f43effdf8001f95be6cea1dede3c2b2
root <root@localhost.localdomain>
2019-11-04 11:23:33 -0500

    testing reverse quotes

--- travel.cmdctrl.ca/iframe.php
+++
@@ -1,8 +0,0 @@
-<html>
-	<head>
-		<title>Welcome: Mercury Tours</title>
-	</head>
-	<body style="margin:0;padding:0;width:100%;height:100%;">
-		<iframe src="https://travel.cmdctrl.ca:8443" scrolling="no" style="width: 100%;height: 100%; overflow: hidden;border: none;"></iframe>
-	</body>
-</html>
